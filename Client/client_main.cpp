88071376f34072aa62e50fb674ac9c7cb3ee293a
bh214 <bh214@duke.edu>
2020-03-30 00:06:28 -0400

    client rand threads success

--- Client/client_main.cpp
+++ Client/client_main.cpp
@@ -27,10 +27,23 @@ void run_client_multiple(Client & client, const std::vector<std::string> & reque
   return;
 }
 
+void run_client_multiple(Client & client,
+                         unsigned int thread_num,
+                         unsigned int bucket_num,
+                         unsigned int delay_min,
+                         unsigned int delay_max) {
+  client.run_multi_thread(thread_num,
+                          bucket_num,
+                          delay_min,
+                          delay_max);
+  return;
+}
+
 int main(int argc, char ** argv) {
-  if (argc != 3 && argc != 4) {
+  if (argc != 3 && argc != 4 && argc != 7) {
     std::string usage = "Usage:./client <server_ip> <server_port>\n";
-    usage += "      ./client <server_ip> <server_port> <test_file_path>";
+    usage += "      ./client <server_ip> <server_port> <test_file_path>\n";
+    usage += "      ./client <server_ip> <server_port> <thread_num> <bucket_num> <delay_min> <delay_max>\n";
 
     std::cerr << usage << std::endl;
     return -1;
@@ -62,6 +75,27 @@ int main(int argc, char ** argv) {
     //   std::cout << requests[i] << std::endl;
     // }
     run_client_multiple(client, requests);
+  }
+  else if (argc == 7) {
+    unsigned int thread_num = 0;
+    unsigned int bucket_num = 0;
+    unsigned int delay_min = 0;
+    unsigned int delay_max = 0;
+    try {
+      thread_num = String_Tools::str_to_unsigned_int(argv[3]);
+      bucket_num = String_Tools::str_to_unsigned_int(argv[4]);
+      delay_min = String_Tools::str_to_unsigned_int(argv[5]);
+      delay_max = String_Tools::str_to_unsigned_int(argv[6]); 
+    }
+    catch (parse_unsigned_int_exception * e) {
+      std::cout << e->what() << std::endl;
+      return EXIT_FAILURE;
+    }
+    run_client_multiple(client,
+                        thread_num,
+                        bucket_num,
+                        delay_min,
+                        delay_max);
   }
   else {
     run_client_single(client);
